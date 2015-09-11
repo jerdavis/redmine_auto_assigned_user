19d9ffd4f0ac7dfea6f571caedd0a9fd067b2e48
jerdavis <jer@advantagelabs.com>
2015-09-11 15:04:26 -0500

    Update hooks.rb
    Removing conditional statement: unless context[:issue].project.members.find(:all, :conditions => ["user_id = ? AND role_id IN (?)", User.current.id, @settings['client_roles'].collect(&:to_i)], :include => [:user, :roles]).first.blank? # If the user is the client, fill the "assigned to" field
    
    That behavior wasn't required for us, and something about the condition is causing a failure state.

--- lib/auto_assigned_user/hooks.rb
+++ lib/auto_assigned_user/hooks.rb
@@ -13,11 +13,9 @@ module AutoAssignedUser
         @settings ||= Setting.plugin_redmine_auto_assigned_user
         if context[:params][:issue]
           if context[:params][:issue][:assigned_to_id].blank?
-            unless context[:issue].project.members.find(:all, :conditions => ["user_id = ? AND role_id IN (?)", User.current.id, @settings['client_roles'].collect(&:to_i)], :include => [:user, :roles]).first.blank? # If the user is the client, fill the "assigned to" field
-                users_list = context[:issue].project.users_by_role
-                manager_role = Role.find(@settings['project_manager_role'].to_i)
-                context[:issue].assigned_to_id = users_list[manager_role].first.id unless users_list[manager_role].blank?
-            end
+            users_list = context[:issue].project.users_by_role
+            manager_role = Role.find(@settings['project_manager_role'].to_i)
+            context[:issue].assigned_to_id = users_list[manager_role].first.id unless users_list[manager_role].blank?
           end
         end
       end
